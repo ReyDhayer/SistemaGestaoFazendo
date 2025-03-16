5a8789660f2b6075291207b6a3506c15b7a903eb
Dhayer <dhayerfort@gmail.com>
2025-03-16 11:59:00 -0300

    Configuração para GitHub Pages

--- retail-magic-manager-main/vite.config.ts
+++ retail-magic-manager-main/vite.config.ts
@@ -3,19 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "localhost",
-    port: 8081,
-    strictPort: false,
-    open: true,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
+  base: '/SistemaGestaoFazendo/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
