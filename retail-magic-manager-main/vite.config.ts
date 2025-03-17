2369124829fa72261d94d1a464d95f2a82cde3d4
Dhayer <dhayerfort@gmail.com>
2025-03-16 21:13:36 -0300

    fix: corrige configurações para deploy no Vercel

--- retail-magic-manager-main/vite.config.ts
+++ retail-magic-manager-main/vite.config.ts
@@ -5,11 +5,18 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
     chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
   },
   server: {
     host: true,
